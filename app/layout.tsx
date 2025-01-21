1608bfde0b31431887d238fb353c1e94ac5765fa
fsdFelipe <devfsdfelipe@gmail.com>
2025-01-21 15:53:46 -0300

    Projeto Criado + estrutura inicial com shadcn-ui

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Next.js e Authjs",
+  description: "Autenticação com next 15 e auth.js v5",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <main className="w-full min-h-screen h-screen bg-gradient-to-b from-zinc-600 to-cyan-400 flex flex-col items-center justify-center">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center gap-4">
+    <h1 className="text-2xl font-extrabold text-center">
+      Home Page
+      <p className="text-md font-normal text-gray-200">Bem vindo !</p>
+    </h1>
+    <Button variant='secondary'>Sign In</Button>
+  </div>
+  );
+}
