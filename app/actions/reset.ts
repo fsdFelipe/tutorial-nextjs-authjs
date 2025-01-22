50c1a4883093be7de64a3c1c09d9e0943c17d084
fsdFelipe <devfsdfelipe@gmail.com>
2025-01-22 10:37:07 -0300

    Resetar senha parte III : enviar email para resetar a senha

--- app/actions/reset.ts
+++ app/actions/reset.ts
@@ -3,6 +3,7 @@ import * as z from "zod";
 import { ResetSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 import { generatePasswordResetToken } from "@/lib/tokens";
+import { sendPasswordResetEmail } from "@/lib/nodeMailer";
 
 export const reset = async (values: z.infer<typeof ResetSchema>) => {
   const validatedFields = ResetSchema.safeParse(values);
@@ -19,7 +20,8 @@ export const reset = async (values: z.infer<typeof ResetSchema>) => {
   }
   
   const passwordResetToken = await generatePasswordResetToken(email);
-  console.log(passwordResetToken)
+  
+  await sendPasswordResetEmail( passwordResetToken.email, passwordResetToken.token );
   
   return { success: "Reset email enviado!" };
 }
--- lib/nodeMailer.ts
+++ lib/nodeMailer.ts
@@ -26,3 +26,30 @@ export const sendVerificationEmail = async (email: string, token: string) => {
     throw error;
   }
 };
+
+export const sendPasswordResetEmail = async (email: string, token: string) => {
+  const domain = process.env.NEXT_PUBLIC_APP_URL;
+  const resetLink = `${domain}/auth/reset?token=${token}`;
+  const transporter = nodemailer.createTransport({
+    service: 'gmail', // ou outro provedor
+    auth: {
+      user: process.env.EMAIL_USER, // Seu e-mail
+      pass: process.env.EMAIL_PASSWORD, // Senha do app ou do e-mail
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER, // E-mail do remetente
+    to: email, // E-mail do destinatário
+    subject: 'Reset senha',
+    html: `<p>Clique <a href="${resetLink}">aqui</a> para cadastrar nova senha.</p>`,
+  };
+  
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('E-mail enviado para:', email);
+  } catch (error) {
+    console.error('Erro ao enviar e-mail:', error);
+    throw error;
+  }
+}  
